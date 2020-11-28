6e9ed587a2db8011819c7fb3308d4b3c37c383dc
CORDERO <sm201600568@wmsu.edu.ph>
2020-11-28 17:19:55 +0800

    rap

---
+++ lib/models/user.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class CustomClassName{
+
+  final String uid;
+
+  CustomClassName({ this.uid });
+
+}
+
+class UserData {
+
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+
+  UserData({ this.uid, this.name, this.sugars, this.strength });
+
+}
